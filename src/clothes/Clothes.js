77c7ddf513bdf0feb9adfdcf1cf0d6f4b2a8187e
biyakim <w2116@e-mirim.hs.kr>
2023-05-29 21:41:53 +0900

    드래그 해서 눈 붙이기

--- src/clothes/Clothes.js
+++ src/clothes/Clothes.js
@@ -4,6 +4,8 @@ import main02 from '../img/main_02.png'
 import Clothes2 from './Clothes.module.css';
 import head from '../img/avatar/head.png';
 import body from '../img/avatar/body.png'
+import eyes01 from '../img/avatar/eyes/eyes_01.png';
+import Draggable from "react-draggable"; 
 
 const Clothes = () => {
     return (
@@ -17,6 +19,12 @@ const Clothes = () => {
             </div>
             <div className="head">
                 <img className={Clothes2.head} src={head}/>
+            </div>
+            <div className='clothes' style={{backgroundColor: 'lightgray', width: '400px', height: '600px',position:'absolute', marginLeft : '1080px', marginTop : '60px', borderRadius : '50px'}}>
+                <div className='box' style={{backgroundColor : 'white', width : '150px', height : '150px', position : 'absolute', borderRadius : '20px', marginTop : '55px', marginLeft : '50px'}}></div>
+                <Draggable>
+                <img className={Clothes2.eyes01} src={eyes01} draggable="true"/>
+                </Draggable>
             </div>
             
         </div>
