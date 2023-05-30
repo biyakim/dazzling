c0f05e8cda3df04f97722c122da3053d2726a7c4
biyakim <w2116@e-mirim.hs.kr>
2023-05-30 15:57:31 +0900

    세팅

--- src/clothes/Clothes.js
+++ src/clothes/Clothes.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import main03 from '../img/main_03.png'
 import main02 from '../img/main_02.png'
 import Clothes2 from './Clothes.module.css';
@@ -6,12 +6,16 @@ import head from '../img/avatar/head.png';
 import body from '../img/avatar/body.png'
 import eyes01 from '../img/avatar/eyes/eyes_01.png';
 import Draggable from "react-draggable"; 
+import hair01 from '../img/avatar/hair/hair_01.png';
+import eyebrow01 from '../img/avatar/eyebrow/eyebrow_01.png';
+import lips01 from '../img/avatar/lips/lips_01.png';
 
 const Clothes = () => {
+
+
     return (
         <div>
             <div className="img">
-                <img className={Clothes2.main03} src={main03}/>
                 <img className={Clothes2.main02} src={main02}/>
             </div>
             <div className="body">
@@ -20,11 +24,21 @@ const Clothes = () => {
             <div className="head">
                 <img className={Clothes2.head} src={head}/>
             </div>
-            <div className='clothes' style={{backgroundColor: 'lightgray', width: '400px', height: '600px',position:'absolute', marginLeft : '1080px', marginTop : '60px', borderRadius : '50px'}}>
-                <div className='box' style={{backgroundColor : 'white', width : '150px', height : '150px', position : 'absolute', borderRadius : '20px', marginTop : '55px', marginLeft : '50px'}}></div>
+            <div className='clothes' style={{backgroundColor: 'white', width: '970px', height: '600px',position:'absolute', marginLeft : '500px', marginTop : '60px', borderRadius : '50px'}}>
                 <Draggable>
-                <img className={Clothes2.eyes01} src={eyes01} draggable="true"/>
+                <img className={Clothes2.eyes01} src={eyes01}/>
                 </Draggable>
+                <Draggable>
+                <img className={Clothes2.hair01} src={hair01}/>
+                </Draggable>
+                <Draggable>
+                <img className={Clothes2.eyebrow01} src={eyebrow01}/>
+                </Draggable>
+                <Draggable>
+                <img className={Clothes2.lips01} src={lips01}/>
+                </Draggable>
+                
+                
             </div>
             
         </div>
